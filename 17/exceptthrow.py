2869a48b1e9f1435b50b7854adf9e08b2e5d0f94
Jerry Asher <ja.2038@gmail.com>
2016-08-27 16:53:15 -0700

    17

---
+++ 17/exceptthrow.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+# day 17: more exceptions
+
+
+class InputError(Exception):
+    pass
+
+
+class Calculator:
+
+    def __init__(self):
+        pass
+
+    def power(self, n, p):
+        if n < 0 or p < 0:
+            raise InputError("n and p should be non-negative")
+        else:
+            return n**p
+
+myCalculator = Calculator()
+T = int(input())
+for i in range(T):
+    n, p = map(int, input().split())
+    try:
+        ans = myCalculator.power(n, p)
+        print(ans)
+    except Exception as e:
+        print(e)
